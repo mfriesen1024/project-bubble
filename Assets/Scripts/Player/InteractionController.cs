be8d0bfc20118b0090e625d04906dd26a302d9ed
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 19:59:01 -0400

    blergh

--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.Player
 {
     public class InteractionController : MonoBehaviour
     {
-
+        public Action OnInteract;
 
         // Use this for initialization
         void Start()
@@ -14,14 +15,22 @@ namespace Assets.Scripts.Player
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-
+            
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if(TryGetComponent(out Ingredient ingredient))
+            {
+
+            }
+            if(TryGetComponent(out Beaker beaker))
+            {
+
+            }
+            if(TryGetComponent(out Door door))
             {
 
             }
