aa0e1d66e4d489811b9f7684e1b62e7bba2a4acf
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 14:36:05 -0400

    Throw new NotImplementedException();

--- Assets/Scripts/Managers/Helpers/UIHelper.cs
+++ Assets/Scripts/Managers/Helpers/UIHelper.cs
@@ -8,5 +8,36 @@ namespace Assets.Scripts.Managers.Helpers
 {
     internal class UIHelper
     {
+        // Store refs here.
+
+
+        public UIHelper(UIManager uIManager)
+        {
+            // Set refs.
+            InitMainMenu();
+            InitInstructions();
+            InitHud();
+            InitPause();
+        }
+
+        private void InitMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitInstructions()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitHud()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitPause()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
