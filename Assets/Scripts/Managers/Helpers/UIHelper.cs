fc4dba24e2423b4e576b364d445a5e0df246f644
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 14:45:39 -0400

    basic initialization functions

--- Assets/Scripts/Managers/Helpers/UIHelper.cs
+++ Assets/Scripts/Managers/Helpers/UIHelper.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Managers.Helpers
 {
     internal class UIHelper
     {
         // Store refs here.
-
+        UIManager uiman;
+        Canvas mainMenu,instructions,hud,pause;
 
         public UIHelper(UIManager uIManager)
         {
             // Set refs.
+            uiman = uIManager;
+            InitUIElements();
             InitMainMenu();
             InitInstructions();
             InitHud();
             InitPause();
         }
 
+        private void InitUIElements()
+        {
+            Canvas[] elements = uiman.GetComponentsInChildren<Canvas>();
+            mainMenu = elements[0];
+            instructions = elements[1];
+            hud = elements[2];
+            pause = elements[3];
+        }
+
         private void InitMainMenu()
         {
-            throw new NotImplementedException();
+            
         }
 
         private void InitInstructions()
