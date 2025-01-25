77299ae7751dca512bd28fd8201840a2d8258b89
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 19:55:36 -0400

    Add interaction controller and ingredient

---
+++ Assets/Scripts/Items/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    internal class Ingredient:MonoBehaviour
+    {
+        public Action Pop = delegate { };
+
+        [SerializeField] GameObject bubble;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out InteractionController ic))
+            {
+                bubble.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/InteractionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class InteractionController : MonoBehaviour
+    {
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(TryGetComponent(out Ingredient ingredient))
+            {
+
+            }
+        }
+    }
+}
