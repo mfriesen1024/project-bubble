b6f855808016dc51071d9a748427cfb578c13988
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 14:10:11 -0400

    Create UIManager.cs

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class UIManager : MonoBehaviour
+    {
+        public UIState State { get => state; set { ExecuteStateChange(value); state = value; } }
+
+
+        UIState state;
+        public Action<UIState> StateChanged = delegate { };
+        private Canvas[] UIElements;
+
+
+        /// <summary>
+        /// Called on startup, godot naming.
+        /// </summary>
+        internal void Ready()
+        {
+
+        }
+
+        // An internal updater.
+        private void ExecuteStateChange(UIState value)
+        {
+            if (state != value)
+            {
+                // hide all ui elements, then activate by number.
+                Hideall();
+                GameObject uiElement = UIElements[(int)value].gameObject;
+            }
+        }
+
+        private void Hideall()
+        {
+            foreach (var element in UIElements) { element.gameObject.SetActive(false); }
+        }
+    }
+
+    public enum UIState
+    {
+        menu,
+        instructions,
+        hud,
+        pause
+    }
+}
