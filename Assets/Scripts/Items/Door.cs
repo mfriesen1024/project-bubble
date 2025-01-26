a1884fee2ec49fc134e270ef4e12ee6d83b8b792
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-25 22:33:12 -0400

    Updated door prefab

--- Assets/Scripts/Items/Door.cs
+++ Assets/Scripts/Items/Door.cs
@@ -6,11 +6,16 @@ namespace Assets.Scripts.Player
     internal class Door:MonoBehaviour
     {
         public PotionType RequiredPotion;
-        [SerializeField] Collider collider;
+        public Action OnDoorMelt = delegate { };
+        [SerializeField] Collider wallCollider;
 
+        // Is this wonky idk.
         public void DoThings()
         {
-            throw new NotImplementedException();
+            OnDoorMelt();
+
+            // This is temporary.
+            wallCollider.enabled = false;
         }
     }
 }
